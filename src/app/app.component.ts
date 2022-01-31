f7b440f9fce9cda185ea5b793148dfcc64e098ad
Wieland Schöbl <wulkanat@gmail.com>
2022-01-31 13:54:40 +0100

    timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,32 @@
 import {Component, OnInit} from '@angular/core';
 import {io, Socket} from "socket.io-client";
 
+type Vector3 = [number, number, number]; // float
+type RotationApproximate = [number, number, number]; // byte
+
+interface StageUpdateData {
+  time: number; // seconds fraction
+  carData?: {
+    frame: number; // int
+    position: Vector3;
+    rotation: RotationApproximate;
+    velocity: Vector3;
+    throttleInput: number; // sbyte
+    steeringInput: number; // sbyte
+    brakeInput: number; // sbyte
+    handbrakeInput: boolean;
+    gear: number; // byte
+    resetCarThisFrame: boolean;
+    engineCondition: number; // sbyte
+    dirtiness: number; // sbyte
+  }
+}
+
+interface ClientData<T> {
+  user: string;
+  data: T;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,7 +36,7 @@ export class AppComponent implements OnInit {
   client!: Socket;
   connected = false;
   error?: string;
-  users: Map<string, number> = new Map();
+  users: Map<string, StageUpdateData | null> = new Map();
 
   ngOnInit() {
     this.client = io('http://localhost:4593/controllers', {
@@ -18,7 +44,7 @@ export class AppComponent implements OnInit {
       upgrade: false
     });
 
-    this.client.on('userJoined', (user: string) => {this.users.set(user, 0)});
+    this.client.on('userJoined', (user: string) => {this.users.set(user, null)});
     this.client.on('userLeft', (user: string) => {
       this.users.delete(user)
     });
@@ -28,8 +54,9 @@ export class AppComponent implements OnInit {
       this.error = error.message
     });
 
-    this.client.on('timerUpdate', (data: any) => {
+    this.client.on('stageUpdate', (data: ClientData<StageUpdateData>) => {
       this.users.set(data.user, data.data)
+      console.log(data.data)
     });
 
     this.client.connect();
